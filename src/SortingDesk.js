7d78f00773a41e6d1bc53ee5ea251def1ff04466
Miguel Guedes <miguel.a.guedes@gmail.com>
2014-08-11 07:49:24 +0100

    Retrieve descriptors for every every bin id

--- src/SortingDesk.js
+++ src/SortingDesk.js
@@ -11,13 +11,53 @@
 
 var SortingDesk = function(options, callbacks)
 {
-  this.options_ = jQuery.extend(true, {}, options);
-  this.callbacks_ = callbacks;
+  var self = this;
+  
+  this.options = jQuery.extend(true, {}, options);
+  this.callbacks = callbacks;
 
-  console.log("Sorting Desk UI instantiated");
+  callbacks.getBinData(options.primaryContentId)
+    .done(function(bin) { self.initialisePrimaryBin(bin); });
 };
 
 SortingDesk.prototype = {
-  callbacks_: null,
-  options_: null,
+  callbacks: null,
+  options: null,
+
+  initialise: function ()
+  {
+    console.log("Sorting Desk UI instantiated");
+  },
+
+  initialisePrimaryBin: function (bin)
+  {
+    var self = this;
+    
+    if(bin.error) {
+      console.log("Failed to retrieve contents of primary bin");
+      return;
+    }
+
+    if(this.options.secondaryContentIds.length > 0) {
+      this.callbacks.getBinData(this.options.secondaryContentIds[0])
+        .done(function (bin) { self.initialiseSecondaryBin(0, bin); });
+    }
+  },
+
+  initialiseSecondaryBin: function (index, bin)
+  {
+    var self = this;
+    
+    if(bin.error) {
+      console.log("Failed to retrieve contents of secondary bin: ",
+                  this.options.secondaryContentIds[index]);
+      return;
+    }
+
+    if(++index < this.options.secondaryContentIds.length) {
+      this.callbacks.getBinData(this.options.secondaryContentIds[index])
+        .done(function (bin) { self.initialiseSecondaryBin(index, bin); });
+    } else
+      this.initialise();
+  }
 };
