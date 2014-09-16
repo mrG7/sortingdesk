85786c26353a0e39868284c4b4f70496024ae819
Miguel Guedes <miguel.a.guedes@gmail.com>
2014-09-16 11:39:34 +0100

    Fix test issues related to RequireJs
    Issues fixed:
    
    * Jasmine was instantiating its UI twice.
    
    * `setup' sometimes wasn't available to test specs due to `init.js' loaded after
      one or more specs.

---
+++ test/specRunner.js
@@ -0,0 +1,20 @@
+require.config( {
+  baseUrl: '../',
+  paths: {
+    "SortingDesk": "src/main/SortingDesk",
+    "API-SortingDesk": "src/api/api-sorting_desk-mock",
+    "API-Data": "src/api/data/al_ahram.json?",
+    "jQuery": "lib/jquery-2.1.1.min"
+  }
+} );
+
+require( [ "SortingDesk" ], function () {
+  require( [ "spec/init.js" ], function () {
+    require( [ "spec/instance.js",
+               "spec/callbacks.js",
+               "spec/interface.js"
+             ], function () {
+               jasmine.getEnv().execute();
+             } );
+  } );
+} );
