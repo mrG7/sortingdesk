ed54c0ae809af9d885ce861db55ff7aa19e38356
Miguel Guedes <miguel.a.guedes@gmail.com>
2014-11-21 09:20:35 +0000

    Create SortingDesk component

---
+++ src/SortingDesk/SortingDesk.js
@@ -0,0 +1,35 @@
+/**
+ * @file Sorting Desk component.
+ * @copyright 2014 Diffeo
+ *
+ * @author Miguel Guedes <miguel@miguelguedes.org>
+ *
+ * Comments:
+ *
+ *
+ */
+
+
+/*global jQuery, define */
+/*jshint laxbreak:true */
+
+
+/* Compatibility with RequireJs. */
+if(typeof define === "function" && define.amd) {
+  define("SortingDesk", [ "jQuery" ], function() {
+    return SortingDesk;
+  });
+}
+
+
+/**
+ * The Sorting Desk module.
+ *
+ * @returns a "class" constructor that creates a Sorting Desk instance.
+ * */
+var SortingDesk = (function (window, $) {
+
+  return {
+  };
+  
+} )(typeof window == 'undefined' ? this : window, jQuery);
