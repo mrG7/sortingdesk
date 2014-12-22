0258b0b5bb965ac520671fe5fefce1b27e7c35de
Miguel Guedes <miguel.a.guedes@gmail.com>
2014-12-22 17:44:07 +0000

    Don't keep state
    Unfortunately we can't keep the configuration state cached and must instead load
    it whenever needed. A cached configuration state would only work if it was
    implemented in the background "page" and managed there.

--- src/browser/extensions/chrome/js/extension/config.js
+++ src/browser/extensions/chrome/js/extension/config.js
@@ -17,47 +17,38 @@ Config = (function () {
     active: true
   };
 
-  var state_ = null;
-
   
   var load = function (callback) {
-    chrome.storage.local.get('config', function (options) {
+    chrome.storage.local.get('config', function (state) {
       console.log("Configuration loaded");
-
+      
       /* If a configuration state has not been found, save an initial default
        * one. */
-      if(!options.hasOwnProperty("active")) {
-        state_ = $.extend(true, { }, defaults_);
-        save(state_);
+      if(!state.hasOwnProperty("config")) {
+        state = $.extend(true, { }, defaults_);
+        save(state);
       } else
-        state_ = options;
+        state = state.config;
 
       if(callback)
-        callback(state_);
-    });
+        callback(state);
+    } );
   };
 
   var save = function (options, callback) {
     chrome.storage.local.set( { "config": options }, function () {
       console.log("Configuration saved");
       
-      state_ = $.extend(true, { }, options);
-
       if(callback)
         callback();
-    });
-  };
-
-  var get = function () {
-    return state_;
+    } );
   };
 
 
   /* Return public interface. */
   return {
     load: load,
-    save: save,
-    get: get
+    save: save
   };
 })();
 
