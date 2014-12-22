e763a4dcec28b01616b6dabcec33bb6e313d50f7
Miguel Guedes <miguel.a.guedes@gmail.com>
2014-12-22 17:44:38 +0000

    Add options page

---
+++ src/browser/extensions/chrome/js/extension/options.js
@@ -0,0 +1,31 @@
+/**
+ * @file Module responsible for the extension's UI configuration.
+ * @copyright 2014 Diffeo
+ *
+ * @author Miguel Guedes <miguel@miguelguedes.org>
+ *
+ * Comments:
+ *
+ */
+
+
+/*global chrome, $, Config */
+
+
+var Options = (function (window, $) {
+  var initialise_ = function () {
+    Config.load(function (state) {
+      $('#active').prop('checked', state.active);
+      
+      $('#save').click(function () {
+        Config.save( {
+          active: $('#active').is(':checked')
+        } );
+      } );
+
+      console.log("Initialised options page");
+    } );
+  };
+
+  initialise_();
+})(window, $);
