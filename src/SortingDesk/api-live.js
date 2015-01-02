fcd624da459dc95a4ac3df9ff4bd7d6bf3ccb525
Miguel Guedes <miguel.a.guedes@gmail.com>
2015-01-02 12:10:39 +0000

    Allow reference to DossierJS's exported classes to be retrieved

--- src/SortingDesk/api-live.js
+++ src/SortingDesk/api-live.js
@@ -150,6 +150,14 @@ var Api = (function() {
         return ["web", id].join("|");
     };
 
+    var getClass = function (cl)
+    {
+        return DossierJS.hasOwnProperty(cl)
+            && typeof DossierJS[cl] === 'function'
+            ? DossierJS[cl]
+            : null;
+    };
+
     return $.extend({}, qitems.callbacks(), {
         setDomain: setDomain,
         getDomain: getDomain,
@@ -160,7 +168,8 @@ var Api = (function() {
         addLabel: addLabel,
         mergeBins: mergeBins,
         generateId: generateId,
-        makeId: makeId
+        makeId: makeId,
+        getClass: getClass
     });
 } )();
 
