0302a2be5687ed7c42675fca7040c7bc401800d2
Miguel Guedes <miguel.a.guedes@gmail.com>
2015-01-26 20:50:33 +0000

    chrome: implement processing of user initiated selection requests

--- src/browser/extensions/chrome/js/content/embed.js
+++ src/browser/extensions/chrome/js/content/embed.js
@@ -25,8 +25,62 @@ var ChromeExtensionUi = (function ($, std, undefined) {
    * */
   var BackgroundListener = (function () {
 
+    /* Events */
+    var onGetSelection_ = function (request, sender, callback)
+    {
+      if(!std.is_fn(callback)) return;
+      
+      var result = { },
+          val,
+          active = embed_.monitor.active;
+
+      if(active && active.length > 0) {
+        /* Retrieve image's src and clear active drop. */
+        val = active.get(0).src;
+        embed_.monitor.clear();
+
+        if(val) {
+          result.id = result.content = val;
+          result.type = "image";
+
+          console.log("Image selection:", result);
+          callback(result);
+        } else
+          console.error("Unable to retrieve valid `src´ attribute");
+      } else {
+        var sel = window.getSelection();
+        
+        if(sel && sel.anchorNode) {
+          val = sel.toString();
+
+          /* Craft a unique id for this text snippet based on its content,
+           * Xpath representation, offset from selection start, length and,
+           * just to be sure, current system timestamp. This id is
+           * subsequently used to generate a unique and collision free
+           * unique subtopic id. */
+          result.xpath = std.Html.getXpathSimple(sel.anchorNode);
+          result.id = [ val,
+                        result.xpath,
+                        sel.anchorOffset,
+                        val.length,
+                        Date.now() ].join('|');
+
+          result.content = val;
+          result.type = "text";
+          
+          console.log("Text selection:", result);
+          callback(result);
+        } else
+          console.error("No text currently selected");
+      }
+
+      /* Retrieval of selection failed. */
+      callback(null);
+    };
+
     /* Map message operations to handlers. */
     var methods_ = {
+      "get-selection": onGetSelection_
     };
 
     /* Interface */
