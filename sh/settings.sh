7ee2b9a23f8c3d16360c5d3afb357239ea5773fb
Miguel Guedes <miguel.a.guedes@gmail.com>
2015-02-05 10:22:20 +0000

    Add scripts for build and reloading firefox extension

--- sh/settings.sh
+++ sh/settings.sh
@@ -11,3 +11,7 @@ readonly URL_DOSSIER_STACK=http://54.174.195.250:8080
 # Prefix to append to Dossier Stack URL
 # Note: ensure prefix BEGINS with a forward slash but does NOT end with one.
 readonly URL_DOSSIER_STACK_PREFIX=/dossier
+
+# URL of local service responsible for reloading the Sorting Desk Firefox
+# extension
+readonly URL_FIREFOX_RELOAD=http://localhost:8888
