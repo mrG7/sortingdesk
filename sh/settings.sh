2b301e7cf639e7b37d9c9f2154ee56561fd323eb
Miguel Guedes <miguel.a.guedes@gmail.com>
2015-02-05 10:34:40 +0000

    Create a "defaults" settings file

--- sh/settings.sh
+++ sh/settings.sh
@@ -1,17 +1,1 @@
-# settings.sh --- Contains project settings used by Bash scripts.
-#
-# Copyright (c) 2015 Diffeo
-#
-# Comments:
-#
-
-# URL of Dossier stack excluding prefixes
-readonly URL_DOSSIER_STACK=http://54.174.195.250:8080
-
-# Prefix to append to Dossier Stack URL
-# Note: ensure prefix BEGINS with a forward slash but does NOT end with one.
-readonly URL_DOSSIER_STACK_PREFIX=/dossier
-
-# URL of local service responsible for reloading the Sorting Desk Firefox
-# extension
-readonly URL_FIREFOX_RELOAD=http://localhost:8888
+settings.sh.default
