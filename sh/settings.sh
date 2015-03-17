b55660041d7cb5dbff53e0fdd3d813920e311cf2
Miguel Guedes <miguel.a.guedes@gmail.com>
2015-03-17 10:21:32 +0000

    sh: fix header

--- sh/settings.sh
+++ sh/settings.sh
@@ -1,4 +1,4 @@
-# settings.sh.default --- Contains DEFAULT project settings used by Bash scripts.
+# settings.sh.default --- Contains project settings used by Bash scripts.
 #
 # Copyright (c) 2015 Diffeo
 #
