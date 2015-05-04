cfc59cf0826891ba645985acf3e40ca95f734b97
Miguel Guedes <miguel.a.guedes@gmail.com>
2015-05-04 09:10:14 +0100

    sorting-common: fix not setting new root

--- src/sorting-common/sorting_common.js
+++ src/sorting-common/sorting_common.js
@@ -447,8 +447,11 @@
       if(!is_fn(callback))
         throw "Invalid or no callback function specified";
 
-      var nf = new NodeFinder(this.tag_, this.prefix_, newRoot);
-      return callback.call(this);
+      var v, t = this.root_;
+      this.root_ = newRoot;
+      v = callback.call(this);
+      this.root_ = t;
+      return v;
     }
   };
 
