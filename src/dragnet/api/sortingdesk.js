cab3eb2f0efe022aa852093c0b53f69a0fd8e2af
Miguel Guedes <miguel.a.guedes@gmail.com>
2015-07-14 16:39:52 +0100

    Implement Dragnet

---
+++ src/dragnet/api/sortingdesk.js
@@ -0,0 +1,148 @@
+/**
+ * @file Sorting Desk extension dragnet component.
+ * @copyright 2015 Diffeo
+ *
+ * Comments:
+ *
+ */
+
+/*jshint laxbreak:true */
+
+
+this.Dragnet = (function (window, $, std, Api, dn, undefined) {
+
+  /**
+   * @namespace
+   * */
+  dn = dn || { };
+  var api = dn.api;
+
+
+  /* Default options */
+  var defaults = {
+    maxItems: 100
+  };
+
+
+  /**
+   * @class
+   * */
+  api.SortingDesk = function (options)
+  {
+    this.options = $.extend(true, { }, options, defaults);
+    this.api = new Api(options.dossierUrl);
+  };
+
+  api.SortingDesk.prototype.getClasses = function ()
+  {
+    var self = this,
+        api = this.api.foldering,
+        classes = [ ];
+
+    return $.Deferred(function (deferred) {
+      var fail = function () { deferred.reject(); };
+
+      /* For each folder, retrieve list of subfolders. */
+      api.listFolders().then(function (folders) {
+        var nextFolder = function (fi) {
+          if(fi >= folders.length) {
+            console.info("Classes loaded", classes);
+            deferred.resolve(classes);
+            return;
+          }
+
+          var cl = [ ];
+
+          /* For each subfolder, save reference and retrieve a random item from
+           * its collection.*/
+          api.listSubfolders(folders[fi]).then(function (subfolders) {
+            var nextSubfolder = function (si) {
+              if(si >= subfolders.length) {
+                classes.push(cl);
+                nextFolder(++fi);
+                return;
+              }
+
+              api.listItems(subfolders[si]).then(function (items) {
+                var sc = subfolders[si];
+
+                /* The following attribute(s) is added by `Api` and is specific
+                 * to Sorting Desk (the extension). */
+                delete sc.exists;
+
+                if(!std.is_arr(items) || items.length === 0)
+                  console.warn("Skipping subfolder: invalid or no items", sc);
+                else {
+                  sc.item = items[Math.floor(Math.random() * items.length)];
+                  cl.push(sc);
+                }
+
+                nextSubfolder(++si);
+              }, fail);
+            };
+
+            nextSubfolder(0);
+          }, fail);
+        };
+
+        nextFolder(0);
+      }, fail);
+    } ).promise();
+  };
+
+  api.SortingDesk.prototype.computeWeights = function (classes)
+  {
+    var self = this,
+        api = this.api.qitems;
+
+    if(!std.is_arr(classes) || classes.length === 0)
+      throw "Invalid dataset";
+
+    return $.Deferred(function (deferred) {
+      var fail = function () { deferred.reject(); };
+
+      var next = function (ci, si) {
+        if(ci >= classes.length) {
+          console.info("Weights calculated", classes);
+          deferred.resolve(classes);
+          return;
+        }
+
+        var cl = classes[ci];
+        if(si >= cl.length) {
+          next(++ci, 0);
+          return;
+        }
+
+        var sc = cl[si];
+        if(!std.is_obj(sc.item)) {
+          console.warn("Skipping class: invalid or no item", cl);
+          sc.weight = 0;
+          next(ci, ++si);
+          return;
+        }
+
+        api.setQueryContentId(sc.item.content_id);
+        api.getCallbacks().moreTexts(self.options.maxItems)
+          .then(function (data) {
+            sc.weight = data.results.length;
+            console.log(sc);
+            next(ci, ++si);
+          }, fail);
+      };
+
+      next(0, 0);
+    } ).promise();
+  };
+
+
+  /* Register with factory. */
+  api.register("sorting-desk", api.SortingDesk);
+
+  return dn;
+
+} )(this,
+    this.$,
+    this.SortingCommon,
+    this.Api,
+    this.Dragnet);
---
+++ src/dragnet/vis.js
@@ -0,0 +1,195 @@
+/**
+ * @file Sorting Desk extension dragnet component.
+ * @copyright 2015 Diffeo
+ *
+ * Comments:
+ *
+ */
+
+/*jshint laxbreak:true */
+
+
+this.Dragnet = (function (window, $, std, d3, dn, undefined) {
+
+  /**
+   * @namespace
+   * */
+  dn = dn || { };
+
+
+  /* Default options */
+  var defaults = {
+    container:      $(),
+    padding:        1.5,        /* between same-cluster circles */
+    clusterPadding: 6,          /* between different-cluster circles */
+    minRadius:      2,
+    maxRadius:      12,
+    marginText:     5,
+    gravity:        0.02,
+    charge:         0
+  };
+
+
+  /**
+   * @class
+   * */
+  dn.Visualisation = function (options)
+  {
+    this.options = $.extend(true, { }, defaults, options);
+    this.dataset = null;
+    this.vis = null;console.log(this.options);
+  };
+
+  dn.Visualisation.prototype.create = function (dataset)
+  {
+    if(this.vis) throw "Visualisation instance exists";
+    this.dataset = dataset;
+    this.refresh();
+  };
+
+  dn.Visualisation.prototype.refresh = function ()
+  {
+    var self = this,
+        opts = this.options,
+        dataset = this.dataset,
+        w = opts.container.width(),
+        h = opts.container.height();
+
+    var force = this.vis = d3.layout.force()
+          .nodes(dataset.nodes)
+          .size([w, h])
+          .gravity(opts.gravity)
+          .charge(opts.charge)
+          .on("tick", tick)
+          .start();
+
+    var color = d3.scale.category10()
+          .domain(d3.range(1));
+
+    var svg = d3.select(opts.container.get(0)).append("svg")
+          .attr("width", w)
+          .attr("height", h);
+
+    var node = svg.selectAll("g.node")
+          .data(this.dataset.nodes);
+
+    var enter = node.enter().append("g")
+          .attr("class", "node")
+          .on("mouseover", onMouseOver)
+          .on("mouseout", onMouseOut)
+          .on("dblclick", onDoubleClick)
+          .call(force.drag);
+
+    enter.append("svg:circle")
+      .attr("r", function(d) { return d.radius; })
+      .style("fill", function(d) { return color(d.cluster); });
+
+    enter.append("text")
+      .attr("x", function(d) { return d.radius + opts.marginText; })
+	    .attr("dy", ".35em")
+	    .text(function(d) { return d.name || "no-name"; });
+
+    function tick(e) {
+      node
+        .each(cluster(10 * e.alpha * e.alpha))
+        .each(collide(.5))
+        .attr("x", function(d) { return d.x; })
+        .attr("y", function(d) { return d.y; });
+
+      node.attr("transform", function (d) {
+        return "translate(" + [d.x, d.y] + ")";
+      } );
+    }
+
+    // Move d to be adjacent to the cluster node.
+    function cluster(alpha) {
+      return function(d) {
+        var cluster = dataset.clusters[d.cluster];
+        if (cluster === d) return;
+        var x = d.x - cluster.x,
+            y = d.y - cluster.y,
+            l = Math.sqrt(x * x + y * y),
+            r = d.radius + cluster.radius;
+        if (l != r) {
+          l = (l - r) / l * alpha;
+          d.x -= x *= l;
+          d.y -= y *= l;
+          cluster.x += x;
+          cluster.y += y;
+        }
+      };
+    }
+
+    // Resolves collisions between d and all other circles.
+    function collide(alpha) {
+      var quadtree = d3.geom.quadtree(dataset.nodes);
+      return function(d) {
+        var r = d.radius + opts.maxRadius
+              + Math.max(opts.padding, opts.clusterPadding),
+            nx1 = d.x - r,
+            nx2 = d.x + r,
+            ny1 = d.y - r,
+            ny2 = d.y + r;
+
+        quadtree.visit(function(quad, x1, y1, x2, y2) {
+          if (quad.point && (quad.point !== d)) {
+            var x = d.x - quad.point.x,
+                y = d.y - quad.point.y,
+                l = Math.sqrt(x * x + y * y),
+                r = d.radius + quad.point.radius
+                  + (d.cluster === quad.point.cluster
+                     ? opts.padding : opts.clusterPadding);
+
+            if (l < r) {
+              l = (l - r) / l * alpha;
+              d.x -= x *= l;
+              d.y -= y *= l;
+              quad.point.x += x;
+              quad.point.y += y;
+            }
+          }
+
+          return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+        } );
+      };
+    }
+
+    function onMouseOver(d) {
+      node.sort(function (a, b) {
+        if(a !== d) return -1;
+        return 1;
+      } );
+
+      node.attr("class", function (e) {
+        var cl = [ "node" ];
+        if(e.cluster === d.cluster) cl.push("show");
+        if(e === d) cl.push("over");
+        return cl.join(" ");
+      } );
+    }
+
+    function onMouseOut(d) {
+      node.attr("class", "node");
+    }
+
+    function onDoubleClick(d) {
+      console.log("event: double click: ", d);
+    }
+  };
+
+  dn.Visualisation.prototype.destroy = function ()
+  {
+    if(!this.vis) throw "No visualization instance";
+    this.vis.stop();
+    this.options.container.empty();
+    this.options = this.dataset = this.vis = null;
+  };
+
+
+  return dn;
+
+} )(this,
+    this.$,
+    this.SortingCommon,
+    this.d3,
+    this.Dragnet);
